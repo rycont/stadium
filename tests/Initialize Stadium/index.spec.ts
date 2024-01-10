41b6f5463e21dfbbb61ab2fb4f45ebc5b16d6d0a
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-01-10 11:15:51 +0000

    test: initialize stadium

--- tests/Initialize Stadium/index.spec.ts
+++ tests/Initialize Stadium/index.spec.ts
@@ -1,9 +1,31 @@
 import { expect, test } from "@playwright/test";
-import { join } from "path";
 
-console.log(join(import.meta.url, "../index.html"));
+test("Initialize Stadium", async ({ page, browserName }) => {
+  await page.goto("./Initialize%20Stadium/index.html");
+  const stadium = await page.waitForSelector("#stadium");
 
-test("Initialize Stadium", async ({ page }) => {
-  await page.goto(join(import.meta.url, "../index.html"));
-  await expect(page.title()).resolves.toMatch("Document");
+  const style = await stadium.evaluate((el) => {
+    const style = window.getComputedStyle(el);
+    const record = {};
+
+    for (const key of [
+      "position",
+      "width",
+      "height",
+      "--x-ratio",
+      "--y-ratio",
+    ]) {
+      record[key] = style.getPropertyValue(key);
+    }
+
+    return record;
+  });
+
+  expect(style).toEqual({
+    position: "relative",
+    width: "500px",
+    height: "500px",
+    "--x-ratio": "1",
+    "--y-ratio": "1",
+  });
 });
