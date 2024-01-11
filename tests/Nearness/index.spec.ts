5648f47c02a5ddedb5c40598b960058bad01b366
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-01-11 10:34:26 +0000

    test: nearness

---
+++ tests/Nearness/index.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "@playwright/test";
+import { relative } from "path";
+
+test("LineCrossingDetector", async ({ page }) => {
+  const testPath =
+    "/" +
+    relative(process.cwd(), new URL(import.meta.url).pathname).replace(
+      "index.spec.ts",
+      "index.html"
+    );
+
+  const alertMessage = new Promise<string>((resolve) =>
+    page.on("dialog", (dialog) => {
+      dialog.accept();
+      resolve(dialog.message());
+    })
+  );
+
+  await page.goto(testPath);
+  expect(await alertMessage).toBe("Near Enemy!");
+});
